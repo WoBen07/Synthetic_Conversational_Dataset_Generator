aa927e770c349d94cce3e101a4a326e25eea94d0
Ben <benedictwohlgemuth78@gmail.com>
2026-07-13 10:40:08 +0200

    more templates

--- engine/dataset_merger.py
+++ engine/dataset_merger.py
@@ -15,7 +15,7 @@ PROJECT_ROOT = Path(__file__).resolve().parents[1]
 
 
 INPUT_FOLDER = PROJECT_ROOT / "Synthetic_Data"
-OUTPUT_FOLDER = PROJECT_ROOT / "Datasets" / "Test_Dataset_1"
+OUTPUT_FOLDER = PROJECT_ROOT / "Datasets" / "Test_Dataset_2"
 TRAIN_OUTPUT = "train.jsonl"
 VALIDATION_OUTPUT = "validation.jsonl"
 
